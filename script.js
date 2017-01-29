652db3719e5ee55c805050b20adb3e76b9538e60
Heidi Konswah <heidikonswah@gmail.com>
2017-01-30 01:33:54 +0200

    29Jan

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var sessionNum = Number($("#sessionNum").html());
 var breakNum =Number($("#breakNum").html());
-
+var sessionTime = Number($("#sessionTime").html());
 $("#minus5session").click(function(){
   if (sessionNum >= 5){
     sessionNum -= 5;
   }
   $("#sessionNum").html(sessionNum);
+  sessionTime = sessionNum;
+  $("#sessionTime").html(sessionTime);
 });
 
 $("#plus5session").click(function(){
   sessionNum += 5;
   $("#sessionNum").html(sessionNum);
+  sessionTime = sessionNum;
+  $("#sessionTime").html(sessionTime);
 });
 
 $("#minus5break").click(function(){
@@ -23,4 +27,21 @@ $("#minus5break").click(function(){
 $("#plus5break").click(function(){
   breakNum += 5;
   $("#breakNum").html(breakNum);
+});
+
+$("#startBtn").click(function(){
+  sessionTime *= 60;
+  $("#say").html("Keep Working . . .");
+
+    setInterval(function(){
+    if (sessionTime>0){
+     sessionTime -= 1;
+      if (sessionTime%60 >=10){
+        $("#sessionTime").html(Math.floor(sessionTime/60) +":"+ sessionTime%60);
+      } else {
+        $("#sessionTime").html(Math.floor(sessionTime/60) +":0"+ sessionTime%60);
+      }
+    }
+  },1000);
+
 });
