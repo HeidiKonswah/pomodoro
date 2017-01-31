52ad5734e965842ffe80d05fdee228d9c7e151bc
Heidi Konswah <heidikonswah@gmail.com>
2017-02-01 00:01:53 +0200

    31Jan

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
+$(document).ready(function(){
+$("#startBreak").hide();
 var sessionNum = Number($("#sessionNum").html());
 var breakNum =Number($("#breakNum").html());
 var sessionTime = Number($("#sessionTime").html());
+var breakTime = Number($("#breakTime").html());
+
+
+  $("#startBtn").click(function(){
+  sessionTime *= 60;
+  $(".header").hide();
+  $("#say").html("Keep Working . . .");
+   var counter = setInterval(timer,1000);
+
+   function timer(){
+     sessionTime -= 1;
+     if (sessionTime === 0){
+       clearInterval(counter);
+       $("#say").hide();
+       $("#startBreak").show();
+     }
+     if (sessionTime%60 >=10){
+        $("#sessionTime").html(Math.floor(sessionTime/60) +":"+ sessionTime%60);
+      } else {
+        $("#sessionTime").html(Math.floor(sessionTime/60) +":0"+ sessionTime%60);
+      }
+   }
+});
+
+$("#startBreakBtn").click(function(){
+    breakTime *= 60;
+  console.log(breakTime);
+   var breakCounter = setInterval(breakTimer,1000);
+
+   function breakTimer(){
+     breakTime -= 1;
+     if (breakTime === 0){
+       clearInterval(breakCounter);
+       // reset everything here
+       $("")
+     }
+     if (breakTime%60 >=10){
+        $("#breakTime").html(Math.floor(breakTime/60) +":"+ breakTime%60);
+      } else {
+        $("#breakTime").html(Math.floor(breakTime/60) +":0"+ breakTime%60);
+      }
+   }
+});
+
 $("#minus5session").click(function(){
   if (sessionNum >= 5){
     sessionNum -= 5;
@@ -29,19 +75,6 @@ $("#plus5break").click(function(){
   $("#breakNum").html(breakNum);
 });
 
-$("#startBtn").click(function(){
-  sessionTime *= 60;
-  $("#say").html("Keep Working . . .");
 
-    setInterval(function(){
-    if (sessionTime>0){
-     sessionTime -= 1;
-      if (sessionTime%60 >=10){
-        $("#sessionTime").html(Math.floor(sessionTime/60) +":"+ sessionTime%60);
-      } else {
-        $("#sessionTime").html(Math.floor(sessionTime/60) +":0"+ sessionTime%60);
-      }
-    }
-  },1000);
 
 });
